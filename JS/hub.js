e2e4504227b981617155ea6927b86560c7f1c96f
Riccardo Granelli <sriccardo.granelli@itis.pr.it>
2026-01-17 16:39:20 +0100

    Preparate API clientside relative a hub.js

--- JS/hub.js
+++ JS/hub.js
@@ -17,18 +17,23 @@ const btnCambiaForm = document.getElementById("btnChangeUsername");
 const btnAnnullaForm = document.getElementById("btnUndoUsername");
 const inputNomeForm = formProfilo.querySelector("input");
 const divBottoniForm = document.getElementById("divButtons");
+const errCambiaInfo = document.getElementById("errChange");
 
-const riga = `<tr>
-                <td class="px-4 py-2 text-left">STANZA #1</td>
-                <td class="px-4 py-2 text-left">#0000</td>
-                <td class="px-4 py-2 text-left">Giocatore</td>
-                <td class="px-4 py-2 text-right">1/4</td>
-                <td class="px-4 py-2 text-right">
-                  <button class="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-500">
-                    Entra
-                  </button>
-                </td>
-              </tr>`
+const regexUsername = /^.{5,20}$/;
+
+const divStanze = document.getElementById("divStanze")
+const riga = `
+    <tr>
+        <td class="px-4 py-2 text-left">STANZA #1</td>
+        <td class="px-4 py-2 text-left">#0000</td>
+        <td class="px-4 py-2 text-left">Giocatore</td>
+        <td class="px-4 py-2 text-right">1/4</td>
+        <td class="px-4 py-2 text-right">
+            <button class="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-500">
+            Entra
+            </button>
+        </td>
+    </tr>`
 
 const giocatore = {
     username: "NomeGiocatore",
@@ -40,19 +45,53 @@ const modificheTemp = {
     img: null
 }
 
- fetch('/api/user')
-     .then(response => response.json())
-     .then(data => {
-         giocatore.username = data.user.username;
-         giocatore.img = data.user.img;
+fetch('/api/user')
+    .then(response => response.json())
+    .then(data => {
+        giocatore.username = data.user.username;
+        giocatore.img = data.user.img;
 
-         refreshUsername();
-         refreshIMG();
-     })
-     .catch(error => {
-         console.error('Non è stato possibile ottenere i dati utente', error);
- });
+        refreshUsername();
+        refreshIMG();
+    })
+    .catch(error => {
+        console.error('Non è stato possibile ottenere i dati utente', error);
+    });
 
+aggiornaStanza();
+
+setInterval(aggiornaStanza, 15000);
+
+function aggiornaStanza() {
+    fetch('/api/stanze')
+        .then(response => response.json())
+        .then(data => {
+            caricaStanze(data.stanze);
+        })
+        .catch(error => {
+            console.error('Non è stato possibile caricare le stanze', error);
+        })
+}
+
+function caricaStanze(stanze) {
+    divStanze.innerHTML = "";
+    stanze.forEach(stanza => {
+        divStanza = `
+            <tr>
+                <td class="px-4 py-2 text-left">${stanza.nome}</td>
+                <td class="px-4 py-2 text-left">${stanza.id}</td>
+                <td class="px-4 py-2 text-left">${stanza.host}</td>
+                <td class="px-4 py-2 text-right">${stanza.numGiocatori}</td>
+                <td class="px-4 py-2 text-right">
+                    <button class="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-500 disabled:bg-gray-600 disabled:text-gray-200 disabled:cursor-not-allowed disabled:hover:bg-gray-400" ${stanza.numGiocatori >= 4 ? "disabled" : ""} data-id="${stanza.id}">
+                        Entra
+                    </button>
+                </td>
+            </tr>`
+        
+        divStanze.innerHTML += divStanza;
+    })
+}
 
 function refreshIMG() {
     let src = giocatore.img;
@@ -65,6 +104,61 @@ function refreshUsername() {
     namePreviewProfile.innerHTML = nome;
     document.getElementById("inNickname").value = nome;
 }
+
+// Nasconde errore del nome
+function nascondiErroreUser() {
+    const errore = btnCambiaForm.nextElementSibling;
+    if (errore.classList.contains("hidden")) return;
+    errore.classList.add("hidden");
+}
+
+// Mostra errore del nome
+function mostraErroreUser() {
+    const errore = btnCambiaForm.nextElementSibling;
+    errore.classList.remove("hidden")
+    setTimeout(nascondiErroreUser, 5000);
+}
+
+function apriModifiche() {
+    divBottoniForm.classList.remove("hidden");
+}
+
+function chiudiModifiche() {
+    imgProfiloProfilo.src = giocatore.img;
+    // inputNomeForm.value = giocatore.username;
+
+    modificheTemp.img = giocatore.img;
+    modificheTemp.username = giocatore.username;
+
+    divBottoniForm.classList.add("hidden");
+
+    disattivaInput();
+    nascondiErroreUser();
+}
+
+function attivaInput() {
+    inputNomeForm.disabled = false;
+    btnCambiaForm.classList.add("hidden");
+    btnAnnullaForm.classList.remove("hidden");
+}
+
+function disattivaInput() {
+    inputNomeForm.disabled = true;
+    inputNomeForm.value = giocatore.username;
+    btnCambiaForm.classList.remove("hidden");
+    btnAnnullaForm.classList.add("hidden");
+}
+
+
+divStanze.addEventListener("click", (e) => {
+    e.stopPropagation();
+    const action = e.target.dataset.id;
+    if (!action) return;
+
+    console.log("Entra nella stanza:", action);
+    
+    fetch(`/game/joinLobby?lobbyId=${action}`);
+});
 
 // Evento per aprire/chiudere la tendina con click diretti
 btnProfiloNav.addEventListener("click", (e) => {
@@ -174,66 +268,56 @@ formProfilo.addEventListener("reset", (e) => {
 formProfilo.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    let nome = e.target.elements["username"].value
-    if (nome.length > 1) {
-        // fetch
-        giocatore.username = nome;
+    let nome = e.target.elements["username"].value;
 
-        console.log("a")
-        if (giocatore.img !== modificheTemp.img && modificheTemp.img != null) {
-            giocatore.img = modificheTemp.img;
-            refreshIMG();
-        }
-        refreshUsername();
+    let userCambiato = nome !== giocatore.username;
+    let imgCambiato = giocatore.img !== modificheTemp.img && modificheTemp.img != null;
 
+    // Se l'utente non ha cambiato nulla
+    if (!userCambiato && !imgCambiato) {
         chiudiModifiche();
+        return;
     }
-    else {
-        mostraErrore();
+    // Se l'utente inserisce un nome non valido mostro l'errore
+    if (!regexUsername.test(nome)) {
+        mostraErroreUser();
+        return;
     }
+
+    const dati = {}
+    if (userCambiato) {
+        dati.username = nome;
+    }
+    if (imgCambiato) {
+        dati.img = modificheTemp.img;
+    }
+
+    fetch('/api/changeUsername', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({dati})
+    })
+        .then(response => response.json())
+        .then(result => {
+            if (result.valido) {
+                giocatore.username = nome;
+                giocatore.img = modificheTemp.img;
+
+                modificheTemp.img = null;
+                refreshUsername();
+                refreshIMG();
+                errCambiaInfo.classList.add("hidden");
+            }
+            else {
+                errCambiaInfo.classList.remove("hidden");
+            }
+
+        })
+        .catch(error => {
+            console.error('Errore invio api', error);
+            errCambiaInfo.classList.remove("hidden");
+        });
 
 });
-
-// Nasconde errore del nome
-function nascondiErrore() {
-    const errore = btnCambiaForm.nextElementSibling;
-    if (errore.classList.contains("hidden")) return;
-    errore.classList.add("hidden");
-}
-
-// Mostra errore del nome
-function mostraErrore() {
-    const errore = btnCambiaForm.nextElementSibling;
-    errore.classList.remove("hidden")
-    setTimeout(nascondiErrore, 5000);
-}
-
-function apriModifiche() {
-    divBottoniForm.classList.remove("hidden");
-}
-
-function chiudiModifiche() {
-    imgProfiloProfilo.src = giocatore.img;
-    // inputNomeForm.value = giocatore.username;
-
-    modificheTemp.img = giocatore.img;
-    modificheTemp.username = giocatore.username;
-
-    divBottoniForm.classList.add("hidden");
-
-    disattivaInput();
-    nascondiErrore();
-}
-
-function attivaInput() {
-    inputNomeForm.disabled = false;
-    btnCambiaForm.classList.add("hidden");
-    btnAnnullaForm.classList.remove("hidden");
-}
-
-function disattivaInput() {
-    inputNomeForm.disabled = true;
-    inputNomeForm.value = giocatore.username;
-    btnCambiaForm.classList.remove("hidden");
-    btnAnnullaForm.classList.add("hidden");
-}
